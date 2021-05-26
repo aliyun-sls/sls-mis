99b997ef669274f55e86ea3c27e8aa81e824c76d
qiansheng91 <qiansheng.zx@gmail.com>
2021-05-26 15:17:07 +0800

    Adjust project

---
+++ demos/sls-mall/shipping-master/startup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -x
+java -javaagent:/sls-mall/opentelemetry-javaagent-all.jar -Dotel.exporter.otlp.endpoint=${ENDPOINT} \
+  -Dotel.exporter.otlp.compression=gzip -Dotel.exporter.otlp.protocol=grpc \
+  -Dotel.exporter.otlp.headers=x-sls-otel-project=${PROJECT},x-sls-otel-instance-id=${INSTANCE},x-sls-otel-ak-id=${ACCESS_KEY_ID},x-sls-otel-ak-secret=${ACCESS_SECRET} \
+  -Dotel.resource.attributes=service.name=${SERVICE_NAME},service.version=${SERVICE_VERSION},host.name=${SERVICE_HOST} -jar app.jar
