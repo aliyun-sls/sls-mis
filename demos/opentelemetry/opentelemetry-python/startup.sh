5f02c2958f512846b26f55f09f907210e80613b5
qiansheng91 <qiansheng.zx@gmail.com>
2021-05-26 12:00:41 +0800

    Improve the opentelemetry-python documentation

--- demos/opentelemetry/opentelemetry-python/startup.sh
+++ demos/opentelemetry/opentelemetry-python/startup.sh
@@ -8,7 +8,7 @@ function checkParam() {
 }
 
 checkParam "${ACCESS_KEY_ID}" "ACCESS_KEY_ID" && checkParam "$ACCESS_SECRET" "ACCESS_SECRET" && checkParam "$PROJECT" "PROJECT" &&
-  checkParam "${LOGSTORE}" "LOGSTORE" && checkParam "${HTTPS_ENDPOINT}" "HTTPS_ENDPOINT"
+  checkParam "${INSTANCE}" "INSTANCE" && checkParam "${ENDPOINT}" "ENDPOINT"
 
 
-poetry run start --service_name=${SERVICE_NAME} --service_version=${SERVICE_VERSION} ${ACCESS_KEY_ID} ${ACCESS_SECRET} ${PROJECT} ${LOGSTORE} ${HTTPS_ENDPOINT}
+poetry run start --service-name=${SERVICE_NAME} --service-version=${SERVICE_VERSION} ${ACCESS_KEY_ID} ${ACCESS_SECRET} ${PROJECT} ${INSTANCE} ${ENDPOINT}
