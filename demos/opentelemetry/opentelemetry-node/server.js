a6840633d196479e63ecb8137183645e28b69076
qiansheng91 <qiansheng.zx@gmail.com>
2021-06-10 18:58:49 +0800

    Refine code

--- demos/opentelemetry/opentelemetry-node/server.js
+++ demos/opentelemetry/opentelemetry-node/server.js
@@ -1,36 +1,51 @@
 "use strict";
 require("./init-trace")(require("./utils")());
 const app = require("express")();
-const { context, getSpan, trace } = require("@opentelemetry/api");
+const {context, getSpan, trace} = require("@opentelemetry/api");
 
 async function sleep(num) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, num);
-  });
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(true);
+        }, num);
+    });
 }
 
 app.get("/hello-world", (req, res, next) => {
-  res.send("hello world");
+    res.send("hello world");
 });
 
-app.get("/save", async (req, res, next) => {
-  const span = getSpan(context.active());
-  const query = req.query;
-  if (query.event) {
-    span.addEvent("custom event : " + query.event);
-    span.setAttribute("event", query.event);
-  }
+app.get("/save-order", async (req, res, next) => {
+    const currentSpan = getSpan(context.active());
 
-  const saveSpan = trace.getTracer().startSpan("Save to db");
-  await sleep(Math.random(10) * 1000);
-  saveSpan.end();
-  res.send("save success");
+    const orderId = req.query.id;
+    res.set("trace-id", currentSpan.context().traceId);
+
+    try {
+        if (orderId == null) {
+            throw Error("Order ID is empty")
+        }
+
+        const childSpan = trace.getTracer("opentelemetry-node").startSpan('SaveOrder/To/DB', {
+            attributes: {
+                orderID: orderId
+            }
+        }, context.active());
+        childSpan.setAttribute("orderInfo", "other information");
+        childSpan.end()
+
+        res.send("Saved")
+    } catch (e) {
+        currentSpan.addEvent("Error with process order", {
+            error: e
+        });
+        res.send("Error with process order");
+    }
 });
+
 
 app.listen(8086, () => {
-  console.log(
-    `Listening on http://localhost:8086. Place visit http://localhost:8086/hello-world`
-  );
+    console.log(
+        `Listening on http://localhost:8086. Place visit http://localhost:8086/hello-world`
+    );
 });
