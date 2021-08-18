52fefca005a85880f505829e6bd23c7e63dd0e85
qiansheng91 <qiansheng.zx@gmail.com>
2021-08-18 20:00:31 +0800

    change debug

--- demos/sls-mall/front-end/api/integral/index.js
+++ demos/sls-mall/front-end/api/integral/index.js
@@ -1,12 +1,12 @@
-(function (){
+(function () {
     'use strict';
 
-    var async     = require("async")
-        , express   = require("express")
-        , request   = require("request")
+    var async = require("async")
+        , express = require("express")
+        , request = require("request")
         , endpoints = require("../endpoints")
-        , helpers   = require("../../helpers")
-        , app       = express();
+        , helpers = require("../../helpers")
+        , app = express();
 
     app.get("/integral", function (req, res, next) {
         console.log("integral Request received with body: " + JSON.stringify(req.body));
@@ -19,13 +19,13 @@
 
         var custId = req.session.customerId;
         let childLogging = req.log.child({"cust_id": custId, 'operation': 'GetUserPoint'});
-        childLogging.info({parameters: req.params, msg: '查看用户可用积分', url: req.url});
+        childLogging.info({parameters: req.params, msg: '查看用户积分', url: req.url});
         async.waterfall([
                 function (callback) {
                     let startTime = Math.floor(Date.now() / 1000);
                     request(endpoints.integralUrl + "/usableIntergral?custId=" + custId, function (error, response, body) {
                         childLogging.info({
-                            msg: "调用[user-points]：获取用户可用积分情况",
+                            msg: "调用[user-points]：获取用户积分情况",
                             isError: error || response.statusCode === 404,
                             body: body,
                             url: endpoints.integralUrl + "/usableIntergral?custId=" + custId,
@@ -52,23 +52,23 @@
             });
     });
     app.get("/all_integral", function (req, res, next) {
-        console.log("all_integral Request received with body: " + JSON.stringify(req.body));
         var logged_in = req.cookies.logged_in;
         if (!logged_in) {
+            req.log.warn("用户没有登陆")
             throw new Error("User not logged in.");
             return
         }
 
         var custId = req.session.customerId;
         let childLogging = req.log.child({"cust_id": custId, 'operation': 'GetAllUserPoints'});
-        childLogging.info({parameters: req.params, msg: '查看用户积分总量', url: req.url});
+        childLogging.info({parameters: req.params, msg: '查看用户积分明细', url: req.url});
 
         async.waterfall([
                 function (callback) {
                     let startTime = Math.floor(Date.now() / 1000);
                     request(endpoints.integralUrl + "/intergralSum?custId=" + custId, function (error, response, body) {
                         childLogging.info({
-                            msg: "调用[user-points]：查看用户积分总量",
+                            msg: "调用[user-points]：查看用户积分明细",
                             isError: error || response.statusCode === 404,
                             body: body,
                             url: endpoints.integralUrl + "/intergralSum?custId=" + custId,
@@ -78,9 +78,7 @@
                         if (error) {
                             return callback(error);
                         }
-                        console.log("Received response: " + JSON.stringify(body));
                         if (response.statusCode == 404) {
-                            console.log("No integral found for user: " + custId);
                             return callback(null, []);
                         }
                         callback(null, JSON.parse(body));
