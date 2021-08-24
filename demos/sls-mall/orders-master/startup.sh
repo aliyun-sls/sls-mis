73560574632e594fe9a1166ccc3ba571d77d18e1
qiansheng91 <qiansheng.zx@gmail.com>
2021-08-24 17:08:16 +0800

    add resource

--- demos/sls-mall/orders-master/startup.sh
+++ demos/sls-mall/orders-master/startup.sh
@@ -3,4 +3,4 @@ set -x
 java -javaagent:/sls-mall/opentelemetry-javaagent-all.jar -Dotel.exporter.otlp.endpoint=${ENDPOINT} \
   -Dotel.exporter.otlp.compression=gzip -Dotel.exporter.otlp.protocol=grpc \
   -Dotel.exporter.otlp.headers=x-sls-otel-project=${PROJECT},x-sls-otel-instance-id=${INSTANCE},x-sls-otel-ak-id=${ACCESS_KEY_ID},x-sls-otel-ak-secret=${ACCESS_SECRET} \
-  -Dotel.resource.attributes=deployment.environment=${DEPLOYMENT_ENVIRONMENT},ervice.name=${SERVICE_NAME},service.version=${SERVICE_VERSION},host.name=${SERVICE_HOST} -jar app.jar
+  -Dotel.resource.attributes=deployment.environment=${DEPLOYMENT_ENVIRONMENT},service.name=${SERVICE_NAME},service.version=${SERVICE_VERSION},host.name=${SERVICE_HOST} -jar app.jar
