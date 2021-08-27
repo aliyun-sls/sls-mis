30eaf5be786e9e18c94f5bf5d7f1ffb185e148db
qiansheng91 <qiansheng.zx@gmail.com>
2021-08-27 14:56:33 +0800

    调整Service Name

--- demos/sls-mall/queue-master-master/startup.sh
+++ demos/sls-mall/queue-master-master/startup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 set -x
+export SERVICE_HOST=`hostname`
 java -javaagent:/sls-mall/opentelemetry-javaagent-all.jar -Dotel.exporter.otlp.endpoint=${ENDPOINT} \
   -Dotel.exporter.otlp.compression=gzip -Dotel.exporter.otlp.protocol=grpc \
   -Dotel.exporter.otlp.headers=x-sls-otel-project=${PROJECT},x-sls-otel-instance-id=${INSTANCE},x-sls-otel-ak-id=${ACCESS_KEY_ID},x-sls-otel-ak-secret=${ACCESS_SECRET} \
