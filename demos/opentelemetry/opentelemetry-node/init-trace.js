47ca9dfada2a04baa479d979b5e4deacdbccb970
qiansheng91 <qiansheng.zx@gmail.com>
2021-09-10 10:50:23 +0800

    upgrade ot version

--- demos/opentelemetry/opentelemetry-node/init-trace.js
+++ demos/opentelemetry/opentelemetry-node/init-trace.js
@@ -15,21 +15,30 @@ const {HttpInstrumentation} = require('@opentelemetry/instrumentation-http');
 const {hostname} = require("os");
 
 module.exports = (parameter) => {
-    const provider = new NodeTracerProvider();
-    registerInstrumentations({
-        tracerProvider: provider,
-        instrumentations: [
-            HttpInstrumentation,
-            ExpressInstrumentation,
-        ],
+    const provider = new NodeTracerProvider({
+        resource: new Resource({
+            [SemanticResourceAttributes.SERVICE_NAME]: parameter.service_name,
+            [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: "production",
+            [SemanticResourceAttributes.SERVICE_VERSION]: parameter.version,
+            [SemanticResourceAttributes.HOST_NAME]: hostname()
+        })
     });
+    provider.register();
+    registerInstrumentations({
+        instrumentations: [
+            new HttpInstrumentation(),
+            new ExpressInstrumentation({
+                ignoreLayersType: ['middleware']
+            }),
+        ],
+        tracerProvider: provider,
+    })
     var meta = new grpc.Metadata();
     meta.add('x-sls-otel-project', parameter.project);
     meta.add('x-sls-otel-instance-id', parameter.instance);
     meta.add('x-sls-otel-ak-id', parameter.access_key_id);
     meta.add('x-sls-otel-ak-secret', parameter.access_secret);
     const collectorOptions = {
-        serviceName: parameter.service_name,
         url: parameter.endpoint,
         credentials: grpc.credentials.createSsl(),
         metadata: meta
