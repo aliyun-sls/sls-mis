1f0a8f3bd3a4ca789116615c7f9283861878314b
qiansheng91 <qiansheng.zx@gmail.com>
2021-12-10 15:20:54 +0800

    add promethus jmx exporter

--- demos/sls-mall/orders-master/startup.sh
+++ demos/sls-mall/orders-master/startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -x
 export SERVICE_HOST=`hostname`
-java -javaagent:/sls-mall/jolokia-jvm-1.7.1.jar=port=7777,host=0.0.0.0 -javaagent:/sls-mall/opentelemetry-javaagent-all.jar -Dotel.exporter.otlp.endpoint=${ENDPOINT} \
+java -javaagent:/sls-mall/jmx_prometheus_javaagent-0.16.1.jar=8888:config.yaml -javaagent:/sls-mall/jolokia-jvm-1.7.1.jar=port=7777,host=0.0.0.0 -javaagent:/sls-mall/opentelemetry-javaagent-all.jar -Dotel.exporter.otlp.endpoint=${ENDPOINT} \
   -Dotel.exporter.otlp.compression=gzip -Dotel.exporter.otlp.protocol=grpc \
   -Dotel.exporter.otlp.headers=x-sls-otel-project=${PROJECT},x-sls-otel-instance-id=${INSTANCE},x-sls-otel-ak-id=${ACCESS_KEY_ID},x-sls-otel-ak-secret=${ACCESS_SECRET} \
   -Dotel.resource.attributes=deployment.environment=${DEPLOYMENT_ENVIRONMENT},service.name=${SERVICE_NAME},service.version=${SERVICE_VERSION},host.name=${SERVICE_HOST} -jar app.jar
