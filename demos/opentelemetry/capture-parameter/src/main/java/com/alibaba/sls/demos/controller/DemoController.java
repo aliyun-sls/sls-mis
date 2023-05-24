a742ce7bea2346185dcae6308f451a7bd8a0b128
千乘 <gavin.zx@alibaba-inc.com>
2023-05-24 08:03:15 +0800

    add output trace id

--- demos/opentelemetry/capture-parameter/src/main/java/com/alibaba/sls/demos/controller/DemoController.java
+++ demos/opentelemetry/capture-parameter/src/main/java/com/alibaba/sls/demos/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.alibaba.sls.demos.controller;
 
 import com.alibaba.sls.demos.service.DemoService;
+import io.opentelemetry.api.trace.Span;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,8 @@ public class DemoController {
 
 	@RequestMapping("/capture/{name}")
 	public String capture(@PathVariable String name, @RequestParam(required = false) String captureParam,
-		@RequestParam(required = false) String donotCaputureParam) {
+		@RequestParam(required = false) String donotCaputureParam, HttpServletResponse response) {
+		response.setHeader("traceId", Span.current().getSpanContext().getTraceId());
 		return demoService.capture(String.format("%s, %s", captureParam, donotCaputureParam));
 	}
 
